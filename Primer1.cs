cd6014cc064c306d3321fdaab7e51ff1eac26164
Oleg <tmr777@mail.ru>
2022-10-07 20:12:53 +0300

    файл на отправку3

--- Primer1.cs
+++ Primer1.cs
@@ -1,8 +1,15 @@
-﻿//Программа выводит вторую цифру трёхзначного числа.
+﻿//Является ли второе число кратным первого.Вариант 2
 
-int num = new Random().Next(100, 999);
+Console.WriteLine("Введите число:"); 
+int num1 = int.Parse(Console.ReadLine()!);
 
-int num1 = num/10;
-int nam2 = num1%10;
+int num2 = new Random().Next(1, 10);
+Console.WriteLine($"Число 2: {num2}.");
 
-Console.WriteLine($"Вторая цифра числа {num} => {nam2}");
+if(num1 % num2 == 0){
+Console.WriteLine($"Число {num2} кратно {num1}.");
+}
+
+else{
+Console.WriteLine($"Число {num2} не является кратным {num1}."); 
+}
