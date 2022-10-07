2ee0c3bc9eedb912ea9affabaca5b9fa95e7d6b1
Oleg <tmr777@mail.ru>
2022-10-07 20:01:58 +0300

    файл на отправку

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿//Найти большее число в случайно выбранном.
+
+int num = new Random().Next(10, 100);
+
+int num1 = num/10;
+int num2 = num%10;
+int max = num1;
+
+if(num2< max){
+    max = num1;
+}
+
+else{
+    max = num2;
+}
+
+Console.WriteLine($"В числе {num} max число {max}");
