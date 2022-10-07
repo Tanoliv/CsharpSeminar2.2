56854e16af876ac4aef4355f5a40399d14cec2ae
Oleg <tmr777@mail.ru>
2022-10-07 20:45:38 +0300

    Варинт3

--- Primer1.cs
+++ Primer1.cs
@@ -1,15 +1,16 @@
 ﻿//Является ли второе число кратным первого.Вариант 2
 
-Console.WriteLine("Введите число:"); 
+Console.Write("Введите первое число:"); 
 int num1 = int.Parse(Console.ReadLine()!);
 
 int num2 = new Random().Next(1, 10);
-Console.WriteLine($"Число 2: {num2}.");
+Console.WriteLine($"Второе число: {num2}.");
+int num = num1 % num2;
 
 if(num1 % num2 == 0){
 Console.WriteLine($"Число {num2} кратно {num1}.");
 }
 
 else{
-Console.WriteLine($"Число {num2} не является кратным {num1}."); 
+Console.WriteLine($"Число {num2} не является кратным {num1} Остаток {num}."); 
 }
